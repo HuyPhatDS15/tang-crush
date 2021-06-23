7e26f10e313933407d73dbbc912891cf24e8b1c4
HuyPhatDS15 <76828589+HuyPhatDS15@users.noreply.github.com>
2021-06-23 15:46:26 +0700

    Update config.js

--- config.js
+++ config.js
@@ -9,11 +9,11 @@ const CONFIG = {
     desc: 'Phải chăng em đã say ngay từ lúc thấy nụ cười ấy ',
     btnYes: 'Vẫn cứ là thích anh <33',
     btnNo: 'Không, Anh trai à :3',
-    question:'Trên thế giới hơn 7 tỉ người mà sao em lại yêu anh <3',
-    btnReply: 'Gửi cho anh <3',
+    question:'Sao m lại nhắn tin với thằng nhạt nhẽo như tao <3',
+    btnReply: 'Yêu t đúng ko <3',
     reply: 'Yêu thì yêu mà không yêu thì yêu <33333333',
     mess: 'Anh biết mà 🥰. Yêu em nhiều nhiều 😘😘',
     messDesc: 'Tối nay 9h a call nhé.',
-    btnAccept: 'Okiiiii lun <3',
-    messLink: 'http://fb.com/PhatDS15A/">PhatDS15A' //link mess của các bạn. VD: https://m.me/nam.nodemy
+    btnAccept: 'Ok nhé <3',
+    messLink: 'http://fb.com/PhatDS15A/' //link mess của các bạn. VD: https://m.me/nam.nodemy
 }
