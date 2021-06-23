b868f7d39e9c573249d7e760a614f4d373d430b2
HuyPhatDS15 <76828589+HuyPhatDS15@users.noreply.github.com>
2021-06-23 15:57:57 +0700

    Update config.js

--- config.js
+++ config.js
@@ -14,6 +14,6 @@ const CONFIG = {
     reply: 'Yêu thì yêu mà không yêu thì yêu <33333333',
     mess: 'Anh biết mà 🥰. Yêu em nhiều nhiều 😘😘',
     messDesc: 'Tối nay 9h a call nhé.',
-    btnAccept: 'Ok nhé <3',
+    btnAccept: 'Ok nhé  <3',
     messLink: 'http://fb.com/PhatDS15A/' //link mess của các bạn. VD: https://m.me/nam.nodemy
 }
